71ec1ad65f171754f8be7e44ab19489d51b0542d
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-05 21:56:53 +0530

    Initial Commit

---
+++ Count My Coffee/Count_My_CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Count_My_CoffeeApp.swift
+//  Count My Coffee
+//
+//  Created by Atin Agnihotri on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Count_My_CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
