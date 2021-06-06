c157890e5c1c4d93049f7b244f94609904ca117b
AtinAgnihotri <atinagnihotri@gmail.com>
2021-06-06 11:33:05 +0530

    Add Basic Layout

--- Count My Coffee/ContentView.swift
+++ Count My Coffee/ContentView.swift
@@ -10,56 +10,67 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepHours = 8.0
     @State private var wakeUp = Date()
+    @State private var cupsOfCoffee = 1
     
     var body: some View {
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? Date()
-        
-//        let someDate = Date()
-//        let components = Calendar.current.dateComponents([.hour, .minute], from: someDate)
-//        let hours = components.hour ?? 0
-//        let minutes = components.minute ?? 0
-        
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        let dateString = formatter.string(from: Date())
-        
-//        let now = Date()
-//        let tomorrow = Date().addingTimeInterval(86400)
-//        let range  = now ... tomorrow
-        // Swift lets us form one-sided ranges – ranges where we specify either the start or end but not both, leaving Swift to infer the other side.
-        
-        return Form {
-//            Text("Count My Coffee")
-//                .padding()
-//
-//            Stepper (value: $sleepHours, in: 4...12, step: 0.25) {
-//                Text("\(sleepHours, specifier: "%g") hours")
-//            }
-//                .padding()
-            
-            DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
-//                       displayedComponents: .hourAndMinute )
-                .datePickerStyle(WheelDatePickerStyle())
-//                .padding()
-//                .labelsHidden()
-
+        NavigationView {
+            VStack {
+                Text("When do you want to wakeup?")
+                    .font(.headline)
+                    .padding()
+                
+                DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+//                    .datePickerStyle(WheelDatePickerStyle())
+                    .padding()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                    .padding()
+                
+                Stepper(value: $sleepHours, in: 4...12, step: 0.25 ){
+                    Text("\(sleepHours, specifier: "%g") hours")
+                        .padding()
+                }.padding()
+                
+                Text("Daily Coffee Intake")
+                    .font(.headline)
+                    .padding()
+                
+                Stepper(value: $cupsOfCoffee, in: 1...20) {
+                    Text(cupsOfCoffee > 1 ? "\(cupsOfCoffee) cups" : "\(cupsOfCoffee) cup")
+                        .padding()
+                }.padding()
+                
+                Button(action: calculateBedTime, label: {
+                    Text("Calculate")
+                }).padding()
+                
+                Spacer()
+                
+            }.navigationBarTitle("Count My Coffee")
+//            .navigationBarItems(trailing:
+//                Button(action: calculateBedTime, label: {
+//                    Text("Calculate")
+//                })
+//            )
         }
-//        VStack {
-//
-//            DatePicker("Please enter a date", selection: $wakeUp)
-//                .padding()
-//        }
+        .edgesIgnoringSafeArea(.all)
     }
+    
+    func calculateBedTime() {
+        
+    }
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-//        ContentView()
         ContentView()
-                    .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
-                    .previewDisplayName("iPhone 12")
+//        ContentView()
+//                    .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+//                    .previewDisplayName("iPhone 12")
     }
 }
