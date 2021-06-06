38b2c83a9784652a8fcd5c691895a0413418bd29
AtinAgnihotri <atinagnihotri@gmail.com>
2021-06-06 13:21:58 +0530

    Switching VStacks to Sections

--- Count My Coffee/ContentView.swift
+++ Count My Coffee/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepHours = 8.0
     @State private var wakeUp = defaultWakeTime
-    @State private var cupsOfCoffee = 1
+    @State private var cupsOfCoffee = 0
     
     @State private var alertTitle = ""
     @State private var alertMsg = ""
@@ -19,35 +19,39 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack (alignment: .leading, spacing: 0) {
-                    Text("When do you want to wakeup?")
-                        .font(.headline)
-                        .padding()
+                Section (header: Text("When do you want to wakeup?")
+                            .font(.headline)
+                            .padding()) {
                     
                     DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                         .datePickerStyle(WheelDatePickerStyle())
+                    
                 }
                 
-                VStack (alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                        .padding()
+                Section (header: Text("Desired amount of sleep")
+                            .font(.headline)
+                            .padding()) {
                     
                     Stepper(value: $sleepHours, in: 4...12, step: 0.25 ){
                         Text("\(sleepHours, specifier: "%g") hours")
                     }.padding()
+                    
                 }
                 
-                VStack (alignment: .leading, spacing: 0) {
-                    Text("Daily Coffee Intake")
-                        .font(.headline)
-                        .padding()
+                Section (header: Text("Daily Coffee Intake")
+                            .font(.headline)
+                            .padding()) {
                     
-                    Stepper(value: $cupsOfCoffee, in: 1...20) {
-                        Text(cupsOfCoffee > 1 ? "\(cupsOfCoffee) cups" : "\(cupsOfCoffee) cup")
-                    }.padding()
+                    Picker("Daily Coffee Intake", selection: $cupsOfCoffee) {
+                        ForEach(1..<21) {
+                            Text("\($0)")
+                        }
+                    }.labelsHidden()
+                    .pickerStyle(WheelPickerStyle())
+                    
                 }
+                
                 
             }.navigationBarTitle("Count My Coffee")
             .navigationBarItems(trailing:
@@ -83,7 +87,7 @@ struct ContentView: View {
         let model = CoffeeCalculatorModel()
         
         do {
-            let prediction = try model.prediction(wake: totalTime, estimatedSleep: sleepHours, coffee: Double(cupsOfCoffee))
+            let prediction = try model.prediction(wake: totalTime, estimatedSleep: sleepHours, coffee: Double(cupsOfCoffee + 1))
             
             let sleepTime = wakeUp - prediction.actualSleep
             
