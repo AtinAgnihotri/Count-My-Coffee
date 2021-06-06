c065925dad40e1cb7f09b61d9d5902638623b584
AtinAgnihotri <atinagnihotri@gmail.com>
2021-06-06 12:23:21 +0530

    Added Prediction Model

--- Count My Coffee/ContentView.swift
+++ Count My Coffee/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State private var wakeUp = Date()
     @State private var cupsOfCoffee = 1
     
+    @State private var alertTitle = ""
+    @State private var alertMsg = ""
+    @State private var showAlert = false
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -44,22 +48,58 @@ struct ContentView: View {
                 
                 Button(action: calculateBedTime, label: {
                     Text("Calculate")
-                }).padding()
+                })
+                    .padding()
+                    .border(Color.black)
                 
-                Spacer()
+//                Spacer()
                 
             }.navigationBarTitle("Count My Coffee")
-//            .navigationBarItems(trailing:
-//                Button(action: calculateBedTime, label: {
-//                    Text("Calculate")
-//                })
-//            )
+            .navigationBarItems(trailing:
+                Button(action: calculateBedTime, label: {
+                    Text("Calculate")
+                })
+            )
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text(alertTitle), message: Text(alertMsg).font(.headline), dismissButton: .default(Text("OK")))
+            }
         }
         .edgesIgnoringSafeArea(.all)
+        
+    }
+    
+    func getTotalSleepTime() -> Double {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        
+        let hours = (components.hour ?? 0) * 60 * 60
+        let minutes = (components.minute ?? 0) * 60
+        
+        return Double(hours + minutes)
+    }
+    
+    func makePrediction(totalTime: Double) {
+        let model = CoffeeCalculatorModel()
+        
+        do {
+            let prediction = try model.prediction(wake: totalTime, estimatedSleep: sleepHours, coffee: Double(cupsOfCoffee))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            alertTitle = "Your ideal bedtime is . . . "
+            alertMsg = formatter.string(from: sleepTime)
+        } catch {
+            alertTitle = "Error"
+            alertMsg = "Sorry, there was a problem calculating the bedtime"
+        }
     }
     
     func calculateBedTime() {
-        
+        let totalTime = getTotalSleepTime()
+        makePrediction(totalTime: totalTime)
+        showAlert = true
     }
     
     
@@ -68,9 +108,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
 //        ContentView()
-//                    .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
-//                    .previewDisplayName("iPhone 12")
+        ContentView()
+                    .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+                    .previewDisplayName("iPhone 12")
     }
 }
